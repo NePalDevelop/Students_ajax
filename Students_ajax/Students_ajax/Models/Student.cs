01f707e8651d31f078d4fe700f354b4c5ba7a3d2
NePalDevelop <96785307+NePalDevelop@users.noreply.github.com>
2022-03-18 10:04:08 +0300

    v.3

--- Students_ajax/Students_ajax/Models/Student.cs
+++ Students_ajax/Students_ajax/Models/Student.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Students_ajax.Models
+﻿namespace Students_ajax.Models
 {
     public class Student
     {
         public int Id { get; set; }
-
-        [Display(Name = "ФИО студента")]
-        [StringLength(60, MinimumLength = 3)]
+  
         public string Name { get; set; }
 
-        [Display(Name = "Адрес студента")]
-        [EmailAddress]
         public string MailAddress { get; set; }
     }
 }
